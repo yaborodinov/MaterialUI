9d38ffa798c3b48db6b0362798a401774d0ed52a
Alexandr Borodinov <yaborodinov@gmail.com>
2022-08-30 23:48:10 +0300

    investigated Typography

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import { Typography } from '@mui/material'
 
 export default function Create() {
   return (
     <div>
-      Create page
+      <Typography
+        variant="h3"
+        component="h1"
+        color="primary"
+        align="center"
+        gutterBottom
+      >
+        Create a new Note
+      </Typography>
+      <Typography
+        color="secondary"
+        noWrap={true}
+        gutterBottom
+      >
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi mollitia ad odit odio cum illum quisquam soluta minima itaque rerum. Dolorum quam minima mollitia maiores, assumenda error asperiores quae explicabo! Quidem iure suscipit explicabo nam molestias illo doloribus saepe consequatur, numquam dolore dignissimos nostrum commodi?
+      </Typography>
+      <Typography
+        color="textSecondary"
+      >
+        Save
+      </Typography>
     </div>
   )
 }
